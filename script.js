2441f2a41eaed757e989ed50aa5b195b41cb5b95
Josh Rufin <joshrufin@gmail.com>
2017-05-09 13:30:15 -0400

    fizz buzz loop

---
+++ script.js
@@ -0,0 +1,15 @@
+  for (var i = 0, i <= 30, i++){
+
+    if (i % 5 === 0 && i % 3 === 0){
+      console.log('fizzbuzz');
+    }
+    if (i % 5 === 0){
+      console.log('buzz');
+    }
+    if (i % 3 === 0){
+      console.log('fizz');
+    }
+    else {
+      console.log(i);
+    }
+  }
